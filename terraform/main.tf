97b00c3ea0c0033b1c00d179887be385cc993327
NechiHeni <h.nechi@arion-tech.com>
2024-09-04 09:39:49 +0100

    test for the purpose of demo presentation

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_B1ms"
+    vm_size    = "Standard_DS2_v2"
   }
    
   identity {
